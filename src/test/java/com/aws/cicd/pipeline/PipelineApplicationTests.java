00cb3d59bfb18ef15840b563ab94ddb3b43f4b7e
azamat-aminov <azamatmaqsudjonovich@gmail.com>
2024-02-06 18:33:05 +0500

    initial commit

---
+++ src/test/java/com/aws/cicd/pipeline/PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.cicd.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
